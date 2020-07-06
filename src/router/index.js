113ff59942d4ed4f87c858ee7165e732841ed018
MXY156018 <1132022296@qq.com>
2020-07-06 18:51:18 +0800

    '导航栏完成'

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,14 @@ import VueRouter from 'vue-router'
 
 import Login from '../views/Login.vue'
 import Home from '../views/home/index'
+import Welcome from '../views/Welcome'
+import Users from '../views/user/users'
+import Roles from '../views/right/roles'
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
 Vue.use(VueRouter)
 
 const routes = [
@@ -17,7 +24,22 @@ const routes = [
     },
     {
         path: '/home',
-        component: Home
+        component: Home,
+        redirect: '/welcome',
+        children: [
+            {
+                path: '/welcome',
+                component: Welcome
+            },
+            {
+                path: '/users',
+                component: Users
+            },
+            {
+                path:'/roles',
+                component: Roles
+            }
+        ]
     }
 ]
 const router = new VueRouter({
@@ -31,4 +53,5 @@ router.beforeEach((to, from, next) => {
     if (!tokenStr) return next('/login');
     next();
 })
+
 export default router
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
         <div class="login_box">
             <!--头像区域-->
             <div class="avatar_box">
-                <img src="../assets/logo.png" alt="">
+                <img src="../assets/imgs/logo.png" alt="">
             </div>
             <!--登录表单区域-->
             <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" class="login_form">
@@ -13,7 +13,8 @@
                 </el-form-item>
                 <!-- 密码区域 -->
                 <el-form-item prop="password">
-                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"
+                              type="password"></el-input>
                 </el-form-item>
                 <!-- 按钮区域 -->
                 <el-form-item class="btns">
@@ -28,39 +29,38 @@
 <script>
     export default {
         name: "Login",
-        data(){
-            return{
-                loginForm:{
-                    username:'admin',
-                    password:'123456'
+        data() {
+            return {
+                loginForm: {
+                    username: 'admin',
+                    password: '123456'
                 },
-                loginFormRules:{
+                loginFormRules: {
                     //验证用户名是否合法
-                    username:[
-                        {required:true, message:'请输入登录名', trigger: 'blur'},
-                        {min:3, max:10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+                    username: [
+                        {required: true, message: '请输入登录名', trigger: 'blur'},
+                        {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
                     ],
                     //验证密码是否合法
-                    password:[
-                        {required:true, message:'请输入登录密码', trigger: 'blur'},
-                        {min:6, max:15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
+                    password: [
+                        {required: true, message: '请输入登录密码', trigger: 'blur'},
+                        {min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
                     ]
                 }
             }
         },
-        methods:{
+        methods: {
             //点击重置按钮按钮  重置登录表单
-            resetLoginForm(){
+            resetLoginForm() {
                 this.$refs.loginFormRef.resetFields();
             },
-            login(){
-                this.$refs.loginFormRef.validate( valid=>{
-                    if(!valid) return;
-                    this.$http.post('login',this.loginForm).then(res=>{
-                        // console.log(res);
-                        if(res.data.meta.status!=200) return this.$message.error('登录错误');
+            login() {
+                this.$refs.loginFormRef.validate(valid => {
+                    if (!valid) return;
+                    this.$http.post('login', this.loginForm).then(res => {
+                        if (res.data.meta.status != 200) return this.$message.error('登录错误');
                         this.$message.success('登录成功');
-                        window.sessionStorage.setItem('token',res.data.data.token);
+                        window.sessionStorage.setItem('token', res.data.data.token);
                         //跳转
                         this.$router.push('/home');
                     });
@@ -71,11 +71,12 @@
 </script>
 
 <style scoped>
-    .login_container{
+    .login_container {
         background-color: #2b4b6b;
         height: 100%;
     }
-    .login_box{
+
+    .login_box {
         width: 450px;
         height: 300px;
         background-color: #fff;
@@ -83,9 +84,10 @@
         position: absolute;
         left: 50%;
         top: 50%;
-        transform: translate(-50%,-50%);
+        transform: translate(-50%, -50%);
     }
-    .avatar_box{
+
+    .avatar_box {
         height: 130px;
         width: 130px;
         border: 1px solid #000000;
@@ -94,23 +96,26 @@
         box-shadow: 0 0 10px #ddd;
         position: absolute;
         left: 50%;
-        transform: translate(-50%,-50%);
+        transform: translate(-50%, -50%);
         background-color: #fff;
     }
-    .avatar_box img{
+
+    .avatar_box img {
         width: 100%;
         height: 100%;
         border-radius: 50%;
         background-color: #eee;
     }
-    .login_form{
+
+    .login_form {
         position: absolute;
         bottom: 0;
         width: 100%;
         padding: 0 20px;
         box-sizing: border-box;
     }
-    .btns{
+
+    .btns {
         display: flex;
         justify-content: flex-end;
     }
